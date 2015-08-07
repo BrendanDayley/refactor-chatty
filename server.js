c168ded8a5a2f8f344ac68040dfc2efdaa83b236
BrendanDayley <brendandayley@gmail.com>
2015-08-07 15:33:54 -0600

    final submission of this project it does what its supposed to

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-var messages = [{"message": "message", "Hello": "goodbye",}]
+var messages = [{"message": "message"}]
 
 var express = require('express');
 var bodyParser = require('body-parser');
